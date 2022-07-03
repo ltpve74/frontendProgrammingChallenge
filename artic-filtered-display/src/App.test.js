8fe2ba8ff29e58571d873f55438dc3d78a70c1c9
ltpve74 <ltpve74@gmail.com>
2022-07-03 18:57:43 +0200

    added some jest tests

--- artic-filtered-display/src/App.test.js
+++ artic-filtered-display/src/App.test.js
@@ -1,8 +1,17 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders submit button', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const formElementSubmit = screen.getByText(/Submit/i);
+  expect(formElementSubmit).toBeInTheDocument();
+
 });
+
+test('renders textfield', () => {
+  render(<App />);
+  const textField = screen.getByLabelText(/Search Artic/i);
+  expect(textField).toBeInTheDocument();
+});
+
+
